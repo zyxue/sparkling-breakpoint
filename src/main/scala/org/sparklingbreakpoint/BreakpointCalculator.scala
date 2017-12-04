03e703bd2f9ebecbc8e42dbf1d94ab4941bac686
Zhuyi Xue <alfred532008@gmail.com>
2017-12-03 16:45:46 -0800

    replaced Array with ArrayBuffer

--- src/main/scala/org/sparklingbreakpoint/BreakpointCalculator.scala
+++ src/main/scala/org/sparklingbreakpoint/BreakpointCalculator.scala
@@ -1,5 +1,7 @@
 package org.sparklingbreakpoint
 
+import scala.collection.mutable.ArrayBuffer
+
 import org.apache.spark.sql.Encoders
 import org.apache.spark.sql.Encoder
 import org.apache.spark.sql.expressions.Aggregator
@@ -13,10 +15,10 @@ case class PCT(
 )
 
 
-class BreakpointCalculator(depthCutoff:Int=50, maxMolExtent:Int = 50000) extends Aggregator[ThinExtent, Array[PCT], Array[Int]] {
-  def consolidateCoverage(cov: Array[PCT]): Array[PCT] = {
+class BreakpointCalculator(depthCutoff:Int=50, maxMolExtent:Int = 50000) extends Aggregator[ThinExtent, ArrayBuffer[PCT], ArrayBuffer[Int]] {
+  def consolidateCoverage(cov: ArrayBuffer[PCT]): ArrayBuffer[PCT] = {
     val cc = cov.sortBy(_.loc)
-    var newCov = Array[PCT]()
+    var newCov = ArrayBuffer[PCT]()
     for (pct <- cc) {
       if (newCov.length == 0) {
         newCov :+= pct
@@ -38,18 +40,18 @@ class BreakpointCalculator(depthCutoff:Int=50, maxMolExtent:Int = 50000) extends
     newCov
   }
 
-  def zero: Array[PCT] = Array[PCT]()
+  def zero: ArrayBuffer[PCT] = ArrayBuffer[PCT]()
 
-  def reduce(buffer: Array[PCT], extent: ThinExtent): Array[PCT] = {
-    val cov = Array(PCT(extent.Start - 1, 0, 1), PCT(extent.End - 1, 1, 0))
+  def reduce(buffer: ArrayBuffer[PCT], extent: ThinExtent): ArrayBuffer[PCT] = {
+    val cov = ArrayBuffer(PCT(extent.Start - 1, 0, 1), PCT(extent.End - 1, 1, 0))
     buffer ++ cov
   }
 
-  def merge(cov1: Array[PCT], cov2: Array[PCT]): Array[PCT] = {
+  def merge(cov1: ArrayBuffer[PCT], cov2: ArrayBuffer[PCT]): ArrayBuffer[PCT] = {
     cov1 ++ cov2
   }
 
-  def finish(cov: Array[PCT]): Array[Int] = {
+  def finish(cov: ArrayBuffer[PCT]): ArrayBuffer[Int] = {
     val csCov = consolidateCoverage(cov)
     val bp = csCov
       .filter(i => (i.cov >= depthCutoff && i.nextCov < depthCutoff) || (i.cov < depthCutoff && i.nextCov >= depthCutoff))
@@ -60,18 +62,18 @@ class BreakpointCalculator(depthCutoff:Int=50, maxMolExtent:Int = 50000) extends
 
     // remove pair breakpoints that are less than maxMolExtent away
     val bpi = bp2.zipWithIndex
-    val bpOdd:Array[PCT] = bpi.filter(_._2 % 2 == 0).map(_._1) // 0-based: so it's actually Odd element
-    val bpEven:Array[PCT] = bpi.filter(_._2 % 2 == 1).map(_._1)
+    val bpOdd:ArrayBuffer[PCT] = bpi.filter(_._2 % 2 == 0).map(_._1) // 0-based: so it's actually Odd element
+    val bpEven:ArrayBuffer[PCT] = bpi.filter(_._2 % 2 == 1).map(_._1)
     val bpz = bpOdd.zip(bpEven)
       .filter(i => (i._2.loc - i._1.loc < maxMolExtent))
-      .flatMap(pcts => Array(pcts._1, pcts._2))
+      .flatMap(pcts => ArrayBuffer(pcts._1, pcts._2))
     bpz.map(_.loc)
   }
 
   // Specifies the Encoder for the intermediate value type
-  def bufferEncoder: Encoder[Array[PCT]] = Encoders.kryo
+  def bufferEncoder: Encoder[ArrayBuffer[PCT]] = Encoders.kryo
 
   // Specifies the Encoder for the final output value type
   // def outputEncoder: Encoder[Int] = Encoders.scalaInt
-  def outputEncoder: Encoder[Array[Int]] = Encoders.kryo
+  def outputEncoder: Encoder[ArrayBuffer[Int]] = Encoders.kryo
 }
--- src/test/scala/org/sparklingbreakpoint/BreakpointSuite.scala
+++ src/test/scala/org/sparklingbreakpoint/BreakpointSuite.scala
@@ -1,5 +1,7 @@
 package org.sparklingbreakpoint
 
+import scala.collection.mutable.ArrayBuffer
+
 import org.scalatest.FunSuite
 
 import org.junit.runner.RunWith
@@ -8,7 +10,7 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class BreakpointSuite extends FunSuite {
-  def checkCovSum(cov1: Array[PCT], cov2: Array[PCT], expected: Array[PCT]): Unit = {
+  def checkCovSum(cov1: ArrayBuffer[PCT], cov2: ArrayBuffer[PCT], expected: ArrayBuffer[PCT]): Unit = {
     val depthCutoff = 5
     val cc = new BreakpointCalculator(depthCutoff)
     assert(cc.consolidateCoverage(cov1 ++ cov2) === expected)
@@ -16,77 +18,77 @@ class BreakpointSuite extends FunSuite {
 
   test("sum identical coverages") {
     checkCovSum(
-      Array(PCT(-1, 0, 10), PCT(10, 10, 0)),
-      Array(PCT(-1, 0, 10), PCT(10, 10, 0)),
-      Array(PCT(-1,0,20), PCT(10,20,0))
+      ArrayBuffer(PCT(-1, 0, 10), PCT(10, 10, 0)),
+      ArrayBuffer(PCT(-1, 0, 10), PCT(10, 10, 0)),
+      ArrayBuffer(PCT(-1,0,20), PCT(10,20,0))
     )
   }
 
   test("sum coverages with equivalent ranges but not the same depth") {
     checkCovSum(
-      Array(PCT(-1, 0, 10), PCT(10, 10, 0)),
-      Array(PCT(-1, 0, 5), PCT(10, 5, 0)),
-      Array(PCT(-1, 0, 15), PCT(10, 15, 0))
+      ArrayBuffer(PCT(-1, 0, 10), PCT(10, 10, 0)),
+      ArrayBuffer(PCT(-1, 0, 5), PCT(10, 5, 0)),
+      ArrayBuffer(PCT(-1, 0, 15), PCT(10, 15, 0))
     )
   }
 
   test("sum coverages not the same range") {
     checkCovSum(
-      Array(PCT(-1, 0, 10), PCT(10, 10, 0)),
-      Array(PCT(2, 0, 5), PCT(10, 5, 0)),
-      Array(PCT(-1, 0, 10), PCT(2, 10, 15), PCT(10, 15, 0))
+      ArrayBuffer(PCT(-1, 0, 10), PCT(10, 10, 0)),
+      ArrayBuffer(PCT(2, 0, 5), PCT(10, 5, 0)),
+      ArrayBuffer(PCT(-1, 0, 10), PCT(2, 10, 15), PCT(10, 15, 0))
     )
   }
 
   test("sum coverages: one range within another") {
     checkCovSum(
-      Array(PCT(-1, 0, 10), PCT(10, 10, 0)),
-      Array(PCT(2, 0, 5), PCT(8, 5, 0)),
-      Array(PCT(-1, 0, 10), PCT(2, 10, 15), PCT(8, 15, 10), PCT(10, 10, 0))
+      ArrayBuffer(PCT(-1, 0, 10), PCT(10, 10, 0)),
+      ArrayBuffer(PCT(2, 0, 5), PCT(8, 5, 0)),
+      ArrayBuffer(PCT(-1, 0, 10), PCT(2, 10, 15), PCT(8, 15, 10), PCT(10, 10, 0))
     )
   }
 
   test("sum coverages: one range intersects with another") {
     checkCovSum(
-      Array(PCT(-1, 0, 10), PCT(10, 10, 0)),
-      Array(PCT(2, 0, 5), PCT(12, 5, 0)),
-      Array(PCT(-1, 0, 10), PCT(2, 10, 15), PCT(10, 15, 5), PCT(12, 5, 0))
+      ArrayBuffer(PCT(-1, 0, 10), PCT(10, 10, 0)),
+      ArrayBuffer(PCT(2, 0, 5), PCT(12, 5, 0)),
+      ArrayBuffer(PCT(-1, 0, 10), PCT(2, 10, 15), PCT(10, 15, 5), PCT(12, 5, 0))
     )
   }
 
   test("sum coverages with different numbers of PCTs") {
     checkCovSum(
-      Array(PCT(-1, 0, 10), PCT(10, 10, 0)),
-      Array(PCT(2, 0, 5), PCT(6, 5, 2), PCT(12, 2, 0)),
-      Array(PCT(-1, 0, 10), PCT(2, 10, 15), PCT(6, 15, 12), PCT(10, 12, 2), PCT(12, 2, 0))
+      ArrayBuffer(PCT(-1, 0, 10), PCT(10, 10, 0)),
+      ArrayBuffer(PCT(2, 0, 5), PCT(6, 5, 2), PCT(12, 2, 0)),
+      ArrayBuffer(PCT(-1, 0, 10), PCT(2, 10, 15), PCT(6, 15, 12), PCT(10, 12, 2), PCT(12, 2, 0))
     )
   }
 
   test("sum coverages: First cov not starting from -1") {
     checkCovSum(
-      Array(PCT(1, 0, 10), PCT(10, 10, 0)),
-      Array(PCT(2, 0, 5), PCT(6, 5, 2), PCT(12, 2, 0)),
-      Array(PCT(1, 0, 10), PCT(2, 10, 15), PCT(6, 15, 12), PCT(10, 12, 2), PCT(12, 2, 0))
+      ArrayBuffer(PCT(1, 0, 10), PCT(10, 10, 0)),
+      ArrayBuffer(PCT(2, 0, 5), PCT(6, 5, 2), PCT(12, 2, 0)),
+      ArrayBuffer(PCT(1, 0, 10), PCT(2, 10, 15), PCT(6, 15, 12), PCT(10, 12, 2), PCT(12, 2, 0))
     )
   }
 
   test("sum coverages with interleaving PCTs") {
     checkCovSum(
-      Array(PCT(3, 0, 10), PCT(10, 10, 0)),
-      Array(PCT(2, 0, 5), PCT(6, 5, 2), PCT(12, 2, 0)),
-      Array(PCT(2, 0, 5), PCT(3, 5, 15), PCT(6, 15, 12), PCT(10, 12, 2), PCT(12, 2, 0))
+      ArrayBuffer(PCT(3, 0, 10), PCT(10, 10, 0)),
+      ArrayBuffer(PCT(2, 0, 5), PCT(6, 5, 2), PCT(12, 2, 0)),
+      ArrayBuffer(PCT(2, 0, 5), PCT(3, 5, 15), PCT(6, 15, 12), PCT(10, 12, 2), PCT(12, 2, 0))
     )
   }
 
   test("sum coverages with identical ranges not starting from -1") {
     checkCovSum(
-      Array(PCT(3, 0, 10), PCT(10, 10, 0)),
-      Array(PCT(3, 0, 5), PCT(10, 5, 0)),
-      Array(PCT(3, 0, 15), PCT(10, 15, 0))
+      ArrayBuffer(PCT(3, 0, 10), PCT(10, 10, 0)),
+      ArrayBuffer(PCT(3, 0, 5), PCT(10, 5, 0)),
+      ArrayBuffer(PCT(3, 0, 15), PCT(10, 15, 0))
     )
   }
 
-  def checkConsolidatedCoverage(cov: Array[PCT], expected: Array[PCT]): Unit = {
+  def checkConsolidatedCoverage(cov: ArrayBuffer[PCT], expected: ArrayBuffer[PCT]): Unit = {
     val depthCutoff = 5
     val cc = new BreakpointCalculator(depthCutoff)
     assert(cc.consolidateCoverage(cov) === expected)
@@ -94,8 +96,8 @@ class BreakpointSuite extends FunSuite {
 
   test("sum coverages at a single location") {
     checkConsolidatedCoverage(
-      Array(PCT(-1, 0, 1), PCT(0, 1, 2), PCT(1, 2, 0)) ++ Array(PCT(-1, 0, 1), PCT(0, 1, 3), PCT(1, 3, 0)),
-      Array(PCT(-1, 0, 2), PCT(0, 2, 5), PCT(1, 5, 0))
+      ArrayBuffer(PCT(-1, 0, 1), PCT(0, 1, 2), PCT(1, 2, 0)) ++ ArrayBuffer(PCT(-1, 0, 1), PCT(0, 1, 3), PCT(1, 3, 0)),
+      ArrayBuffer(PCT(-1, 0, 2), PCT(0, 2, 5), PCT(1, 5, 0))
     )
   }
 
